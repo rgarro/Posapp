689a8e695e6903991ab1989f38e2033b91ef1b12
rgarro <rgarro@gmail.com>
2024-10-06 10:41:57 -0600

    fume mota los domingos y los lunes un par de joints

---
+++ Posapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posapp
+//
+//  Created by Rolando Garro on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
